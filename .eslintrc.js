c7dabf219b9450a78b058cb01313a73db8bb0b67
Le Thi Thuy Tien <lethithuytien0307@gmail.com>
2020-06-07 22:55:59 +0700

    [master] init branch

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    cmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  plugins: ['prettier'],
+  env: {
+    jest: true,
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  rules: {
+    // Check props type
+    'react/prop-types': 1,
+    'react/jsx-max-props-per-line': 1,
+    // Một số rule trong airbnb ko quan trọng thì tắt nó đi như linebreak-style
+    'linebreak-style': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/jsx-filename-extension': 0,
+    'reat/jsx-one-expression-per-line': 0,
+    'react/forbid-prop-types': 0,
+    'react/require-default-props': 0,
+    'prettier/prettier': ['error'],
+    'no-unused-vars': 1,
+    'import/extensions': 0,
+    'import/order': 1,
+    'react/prefer-stateless-function': 0,
+    'no-use-before-define': 0,
+    'no-param-reassign': 0,
+    'import/no-unresolved': 2,
+    'import/prefer-default-export': 0,
+  },
+};
